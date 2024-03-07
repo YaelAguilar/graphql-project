a74bd4487ddadeb14595e92a209bc3b3f6edb100
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-07 03:30:03 -0600

    [ADD] schema and types

---
+++ src/graphql/schema.js
@@ -0,0 +1,36 @@
+const {GraphQLSchema, GraphQLObjectType} = require ('graphql');
+const { users, user, posts, post, comments, comment } = require('./queries');
+//const { register, login, createPost, updatePost, deletePost, addComment, updateComment, deleteComment } = require('./mutations');
+
+const QueryType = new GraphQLObjectType({
+    name: 'QueryType',
+    description: 'root query',
+    fields: {
+        users, 
+        user,
+        posts,
+        post,
+        comments,
+        comment,
+    },
+});
+
+/*const MutationType = new GraphQLObjectType({
+    name: 'MutationType',
+    description: 'root mutation',
+    fields: {
+        register,
+        login,
+        createPost,
+        updatePost,
+        deletePost,
+        addComment,
+        updateComment,
+        deleteComment,
+    },
+});*/
+
+module.exports = new GraphQLSchema({
+    query: QueryType,
+    //mutation: MutationType,
+});
